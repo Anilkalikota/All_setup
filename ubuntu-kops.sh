39072303b68f547d08a7e2645b6b4b1a44af6b60
Anilkalikota <129193209+Anilkalikota@users.noreply.github.com>
2024-05-28 07:36:37 +0530

    Update ubuntu-kops.sh

--- ubuntu-kops.sh
+++ ubuntu-kops.sh
@@ -14,8 +14,8 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket devopslearn24.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket devopslearn24.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://devopslearn24.k8s.local
+aws s3api create-bucket --bucket devopslearn24new.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket devopslearn24new.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://devopslearn24new.k8s.local
 kops create cluster --name raj.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
 kops update cluster --name raj.k8s.local --yes --admin
